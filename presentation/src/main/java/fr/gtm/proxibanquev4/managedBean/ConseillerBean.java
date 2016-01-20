6cae80977946d10ce9e4644c5e58fe082fa69c1d
coraliemartin17 <coralie.martin17@gmail.com>
2016-01-20 14:55:53 +0100

    Lien présentation et managed bean

---
+++ presentation/src/main/java/fr/gtm/proxibanquev4/managedBean/ConseillerBean.java
@@ -0,0 +1,45 @@
+package fr.gtm.proxibanquev4.managedBean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import org.springframework.stereotype.Component;
+
+@ManagedBean
+@SessionScoped
+@Component
+public class ConseillerBean {
+
+	protected String nom;
+	protected String prenom;
+	protected String login;
+	protected String password;
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
