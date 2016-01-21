6977e90fb3fcec1746c035d32bd2d11b9bf773cc
BenWayne182 <benwayne182@gmail.com>
2016-01-21 11:23:46 +0100

    [cosmetic] clean repository

--- dao-springdata/src/main/java/fr/gtm/proxibanquev4/dao_springdata/directeur/IDirecteurDaoSD.java
+++
@@ -1,9 +0,0 @@
-package fr.gtm.proxibanquev4.dao_springdata.directeur;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fr.gtm.proxibanquev4.domaine.Directeur;
-
-public interface IDirecteurDaoSD extends JpaRepository<Directeur, Integer> {
-
-}
--- dao/src/main/java/fr/gtm/proxibanquev4/dao/directeur/IDirecteurDaoSD.java
+++
@@ -1,9 +0,0 @@
-package fr.gtm.proxibanquev4.dao.directeur;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fr.gtm.proxibanquev4.domaine.Directeur;
-
-public interface IDirecteurDaoSD extends JpaRepository<Directeur, Integer> {
-
-}
