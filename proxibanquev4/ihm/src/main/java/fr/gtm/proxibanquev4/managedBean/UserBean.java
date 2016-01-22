b9a8911583f640671558901decd296f3ae3a6d62
BenWayne182 <benwayne182@gmail.com>
2016-01-22 10:26:54 +0100

    Methode de connexion dans Userbean

--- proxibanquev4/ihm/src/main/java/fr/gtm/proxibanquev4/managedBean/UserBean.java
+++ proxibanquev4/ihm/src/main/java/fr/gtm/proxibanquev4/managedBean/UserBean.java
@@ -3,16 +3,28 @@ package fr.gtm.proxibanquev4.managedBean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.transform.impl.AddDelegateTransformer;
 import org.springframework.stereotype.Component;
+
+import fr.gtm.proxibanquev4.domaine.Conseiller;
+import fr.gtm.proxibanquev4.domaine.Directeur;
+import fr.gtm.proxibanquev4.service.conseiller.IConseillerService;
+import fr.gtm.proxibanquev4.service.directeur.IDirecteurService;
 
 @ManagedBean
 @SessionScoped
 @Component
 public class UserBean {
 
+	@Autowired
+	private IConseillerService servicecons;
+	@Autowired
+	private IDirecteurService servicedir;
+
 	protected String login;
 	protected String password;
-	
+
 	public String getLogin() {
 		return login;
 	}
@@ -25,16 +37,33 @@ public class UserBean {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+
 	
-	public String connection()
-	{
-		if(getLogin().equalsIgnoreCase("toto") && getPassword().equals("toto"))
-		{
-			return "ConseillerAcceuil";
+	public String connection(){
+		Conseiller conseiller = servicecons.readConseillerByLogin(login);
+		Directeur directeur = servicedir.readDirecteurByLogin(login);
+		if(conseiller != null){
+			if(getLogin().equalsIgnoreCase(conseiller.getLogin()) && getPassword().equals(conseiller.getPassword()))
+			{
+				return "ConseillerAcceuil";
+			}
+			else{
+				return "connexion";
+			}
 		}
-		else
-		{
-			return "ConseillerErreur";
+		else if (directeur != null){ 
+			if(getLogin().equalsIgnoreCase(directeur.getLogin()) && getPassword().equals(directeur.getPassword())){
+				return "DirecteurAcceuil";
+			}
+			else{
+			return "connexion";
+			}
 		}
+		else{
+			return "connexion";
+		}
+
+		
 	}
 }
